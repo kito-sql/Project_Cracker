75103c8306800f3cc26cf84b11c1a4e63c5f85a7
kito-sql <yamifromtheblackbulls@gmail.com>
2025-11-24 16:29:12 -0500

    updated

--- cracker.M.c
+++ cracker.M.c
@@ -1,131 +1,80 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
+#include <iostream>
+#include <vector>
 
-typedef struct {
+using namespace std;
+
+struct A {
     int guess[3];
-    int wrong;
     int right;
     int misplaced;
-} Attempt;
+    int wrong;
+};
 
-// how many of each color : red / green / blue ......
+void score_candidate(int code[3], int guess[3], int &right, int &misplaced, int &wrong) {
+    right = misplaced = wrong = 0;
 
-int score_candidate( int code[3] , int guess[3] , int *right, int *misplaced, int *wrong) {
-    
-    int usedC[3] = {0}, usedG[3] = {0};
-    
-    int r = 0, m = 0;
-
-    // count greens : 
-
-    for(int i=0;i<3;i++){
-        if(code[i]==guess[i]){
-            r++;
-            usedC[i]=usedG[i]=1;
+    for (int i = 0; i < 3; i++) {
+        if (code[i] == guess[i]) {
+            right++;
+            guess[i] = -1;
+            code[i] = -1;
         }
     }
 
-    // count blues :
-
-    for(int i=0; i<3 ; i++){
-        if(!usedG[i]){
-            for( int j=0 ; j <3 ;j++ ){
-                if( !usedC[j] && guess[i]==code[j]){
-                    m++;
-                    usedC[j]=1;
-                    usedG[i]=1;
+    for (int i = 0; i < 3; i++) {
+        if (guess[i] != -1) {
+            for (int j = 0; j < 3; j++) {
+                if (code[j] == guess[i] && code[j] != -1) {
+                    misplaced++;
+                    code[j] = -1;
                     break;
                 }
             }
         }
     }
 
-    *right = r;
-    *misplaced = m;
-    *wrong = 3 - r - m;
-    return 1;
+    wrong = 3 - right - misplaced;
 }
 
-int next_int() {
-    int c;
-
-    // separation des entiers ..
-    do {
-        c = getchar();
+int main() {
+    vector<A> A1;
+    string input;
     
-    } while(!isdigit(c));
-
-    int value = 0;
-    while(isdigit(c)) {
-        value = value * 10 + (c - '0');
-        c = getchar();
+    while (getline(cin, input)) {
+        int wrong, right, misplaced, g0, g1, g2;
+        if (sscanf(input.c_str(), "[%d %d %d] %d %d %d", &wrong, &right, &misplaced, &g0, &g1, &g2) == 6) {
+            A guess;
+            guess.right = right;
+            guess.misplaced = misplaced;
+            guess.wrong = wrong;
+            guess.guess[0] = g0;
+            guess.guess[1] = g1;
+            guess.guess[2] = g2;
+            A1.push_back(guess);
+        }
     }
 
-    return value;
-}
+    for (int d0 = 0; d0 <= 9; d0++) {
+        for (int d1 = 0; d1 <= 9; d1++) {
+            for (int d2 = 0; d2 <= 9; d2++) {
+                int code[3] = {d0, d1, d2};
+                bool valid = true;
 
-
-int main(){
-    
-  
-        printf(" your input :\n");
-        Attempt A[50];
-        int count=0;
-        char ch;
-
-        while((ch=getchar()) != '}'){
-            if(ch=='['){
-                // in order : r / g / b ...
-                A[count].wrong     = next_int();  // r
-                A[count].right     = next_int();  // g 
-                A[count].misplaced = next_int();  // b
-
-                // Skip until next '[' or '}'
-                while((ch=getchar())!='[' && ch!='}') if(ch==EOF) break;
-                if(ch=='}') break;
-
-                // sepearation des entiers
-
-                A[count].guess[0] = next_int();  // 1st 
-                A[count].guess[1] = next_int();  // 2nd
-                A[count].guess[2] = next_int();  // 3rd
-
-                count++;
-            }
-        }
-
-        
-
-        int nb_sol=0;
-
-        for( int d0=0 ; d0 <= 9 ; d0++){
-            for(int d1=0 ; d1 <= 9 ; d1++){
-                for(int d2=0 ;d2 <= 9 ; d2++){
-                    int code[3]={d0,d1,d2};
-                    
-                    int valid=1;
-
-                    for( int i=0; i < count; i++){
-                        int r ,m,w ;
-                        score_candidate(code ,A[i].guess, &r ,&m , &w );
-                        if( r!=A[i].right || m!=A[i].misplaced || w!=A[i].wrong){
-                            valid=0;
-                            break;
-                        }
+                for (const auto& guess : A1) {
+                    int r, m, w;
+                    score_candidate(code, guess.guess, r, m, w);
+                    if (r != guess.right || m != guess.misplaced || w != guess.wrong) {
+                        valid = false;
+                        break;
                     }
+                }
 
-                    if(valid)
-                    {
-                        printf("Answer is :[%d %d %d]\n", d0,d1,d2 );
-                        
-                    }
+                if (valid) {
+                    cout << "Possible solution: [" << d0 << " " << d1 << " " << d2 << "]" << endl;
                 }
             }
         }
+    }
 
-    
-    
-
-return 0;
+    return 0;
 }
